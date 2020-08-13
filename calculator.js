fe88c0a93dbfc5771dbc8baf63bf0fbabe0942b3
Claudia Petty <claudialove32@gmail.com>
2020-08-13 15:44:28 -0400

    add bmi calculator

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@ const app = express();
 ////USE BODY PARSER/////////
 app.use(bodyParser.urlencoded({extended:true}));
 
-////ROUTE///////
+////ROUTE SUM CALC///////
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
@@ -20,6 +20,19 @@ var num2 = Number(req.body.n2);
 
 var result = num1 + num2;
 res.send("The result of the calculation is " + result);
+});
+
+//////ROUTES BMI CALC/////
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+var height = Number(req.body.height);
+var weight = Number(req.body.weight); 
+var bmi = Math.floor((703 * weight)/(height * height));
+
+res.send("Your current BMI is " + bmi);
 });
 
 
